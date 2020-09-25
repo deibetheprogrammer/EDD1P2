30196499e3c07e892a23bcf7ec210e22d9ce9172
ZunigaJose <zunigajose21@gmail.com>
2020-09-25 15:13:10 -0600

    Carga de archivos, cambie la funcion a static

--- Tree.cpp
+++ Tree.cpp
@@ -1,5 +1,9 @@
 #include "Tree.hpp"
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <queue>
 using namespace std;
 
 Tree::Tree(int nNodos)
@@ -7,19 +11,20 @@ Tree::Tree(int nNodos)
     this->root = 0;
     for (int i = 0; i < nNodos; i++)
     {
-        nodes.push_back(new TreeNode(nullptr,nullptr,nullptr,NULL));
+        nodes.push_back(new TreeNode(nullptr,nullptr,nullptr,""));
     }
     
 }
 
 TreeNode* Tree::crea(string etiqueta,int nodo, int izq, int der) {
-    nodes[nodo]->setIzq(nodes[izq]);
-    nodes[nodo]->setDer(nodes[der]);
-
-    nodes[izq]->setPadre(nodes[nodo]);
-    nodes[der]->setPadre(nodes[nodo]);
-
-    nodes[nodo]->setEtiqueta(etiqueta);
+    nodes.at(nodo)->setIzq((izq == - 1) ? nullptr : nodes[izq]);
+    nodes.at(nodo)->setDer((der == - 1) ? nullptr : nodes[der]);
+    if (izq != -1)
+        nodes.at(izq)->setPadre(nodes[nodo]);
+    if (der != -1)    
+        nodes.at(der)->setPadre((der == - 1) ? nullptr : nodes[nodo]);
+    nodes.at(nodo)->setEtiqueta(etiqueta);
+    return nullptr;
 }
 
 TreeNode* Tree::padre(TreeNode* tNode) {
@@ -48,4 +53,37 @@ void Tree::anula()
         delete tNode;
     }
     nodes.clear();
+}
+
+Tree Tree::leerDeArchivo(string nombre) {
+    ifstream archivo;
+    archivo.open(nombre + ".txt", ios::in);
+    vector<string> lineas;
+    string linea;
+    getline(archivo, linea);
+    int nLin = stoi(linea);
+    for(int i = 0; i < nLin; i++) {
+        if (getline(archivo, linea)) {
+            lineas.push_back(linea);
+        } else {
+            lineas.push_back("");
+        }        
+    }
+    linea = lineas.at(0);
+    Tree Arbol(nLin);
+    for (int i = 0; i < lineas.size(); i++) {
+        linea = lineas.at(i);
+        if (linea.size() == 3) {
+            Arbol.crea(to_string(i), i, (linea.at(0) - 48), (linea.at(2) - 48));
+        } else if(linea.size() == 2) {
+            Arbol.crea(to_string(i), i, (linea.at(0) - 48));
+        } else {
+            Arbol.crea(to_string(i), i);
+        }
+    }
+    return Arbol;
+}
+
+Tree::~Tree() {
+    anula();
 }
